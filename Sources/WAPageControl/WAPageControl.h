1e90c702d5ad6afe4652f722cd9f4a68208e1f5c
Waqas Ali <waqasaliw@gmail.com>
2021-11-27 12:30:08 +0400

    Source files have added.

---
+++ Sources/WAPageControl/WAPageControl.h
@@ -0,0 +1,18 @@
+//
+//  WAPageControl.h
+//  WAPageControl
+//
+//  Created by EDS on 27/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WAPageControl.
+FOUNDATION_EXPORT double WAPageControlVersionNumber;
+
+//! Project version string for WAPageControl.
+FOUNDATION_EXPORT const unsigned char WAPageControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WAPageControl/PublicHeader.h>
+
+
